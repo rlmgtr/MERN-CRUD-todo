41b2d4214957711e9575dc373fd793633cab894c
rlmgtr <rlmagtira@gmail.com>
2025-01-18 13:40:23 +0800

    working on server
    testing endpoints

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,9 @@
 const express = require('express');
+
+const app = express();
+
+app.get('/todos', (req, res) => {
+    res.send('this is test /todo path');
+});
+
+app.listen(8000);
---
+++ server/src/index2.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const router = require('./router.js');
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('tiny'));
+
+app.get('/', (req, res) => {
+    res.send('this is backebd');
+});
+
+app.use(router);
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+console.log('this is port 8000');
+app.listen(8000);
+});
