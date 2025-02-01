473bac8d0c75b26f51134e530577bc6530455795
GeeksforGeeks <rlmagtira@gmail.com>
2025-02-01 16:32:17 +0800

    server progress - update
    update feature is added and working
    TYSML

--- server/src/router.js
+++ server/src/router.js
@@ -4,17 +4,18 @@ const signUpRoute = require('./routes/signUpRoute');
 const loginRoute = require('./routes/loginRoute');
 const isLoggedIn = require('./middleware/isLoggedIn');
 const createTodoRoute = require('./routes/createTodoRoute');
-const readTodoRoute = require('./routes/readTodoRoute')
+const readTodoRoute = require('./routes/readTodoRoute');
+const updateTodoRoute = require('./routes/updateTodoRoute');
 
 router.use('/signup', signUpRoute);
 
-
 router.post('/login', loginRoute);
-
 
 router.use('/todos',isLoggedIn, createTodoRoute);
 
-
 router.use('/todos',isLoggedIn, readTodoRoute);
+
+router.use('/todos', isLoggedIn, updateTodoRoute);
+
 
 module.exports = router;
