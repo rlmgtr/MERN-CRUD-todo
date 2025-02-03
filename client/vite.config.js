189d30394848334d4c49652d7843f9ecf12fe944
GeeksforGeeks <rlmagtira@gmail.com>
2025-02-04 00:40:11 +0800

    revert changes
    deleted client and gh pages creation related files

--- client/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/MERN-CRUD-todo/', // Set to your repo name
-});
