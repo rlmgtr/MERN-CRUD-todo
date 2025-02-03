39f7be534f45d79ce0f6515b5714c033e5e20d36
GeeksforGeeks <rlmagtira@gmail.com>
2025-02-04 02:29:25 +0800

    updated package.json - added homepage

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Change this to match your repo name
 export default defineConfig({
-  base: '/MERN-CRUD-todo/', // Replace with your GitHub repository name
   plugins: [react()],
+  base: "/MERN-CRUD-todo/",  // Add this line
 });
