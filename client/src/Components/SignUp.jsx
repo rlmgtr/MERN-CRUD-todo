0ed83afec39182ed5a0bbff34c3153286ed69558
GeeksforGeeks <rlmagtira@gmail.com>
2025-02-18 14:05:49 +0800

    Front End progress
    - Sign up can now store user data
    - added confirm password feature upon sign up

--- client/src/Components/SignUp.jsx
+++ client/src/Components/SignUp.jsx
@@ -7,7 +7,8 @@ const SignUp = () => {
 firstName: '',
 lastName: '',
 email: '',
-password: ''
+password: '',
+confirmPassword: '',
   });
 
 const [error, setError] = useState('');
@@ -26,18 +27,35 @@ e.preventDefault();
 setError('');
 setSuccess('');
 
+
+if (SignUpData.password !== SignUpData.confirmPassword) {
+  setError('Passwords do not match');
+  return;
+ }
+
+const { confirmPassword, ...dataToSend } = SignUpData;
+
 try {
 const response = await fetch(`${API_URL}/signup`, {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json'}, 
-body: JSON.stringify(SignUpData)
+body: JSON.stringify(dataToSend),
   });
 
   const data = await response.json();
 
   if (response.ok) { 
-    setSuccess('Registration Success!')
+    setSuccess(data.message || 'Registration Success!');
+
+    setSignUpData({
+firstName: '',
+lastName: '',
+email: '',
+password: '',
+confirmPassword: '',
+    });
+
   } else {
 setError(data.message || 'Registration failed. Please try again');
   }
@@ -50,15 +68,17 @@ setError(data.message || 'Registration failed. Please try again');
 
   return (
     <div>
+      <ul className='signUpinfo'>
       <form onSubmit={handleSubmit}>
-      First Name <input type='text' name='firstName' placeholder='First Name' value={SignUpData.firstName} onChange={handleChange}/>
-      Last Name <input type='text' name='lastName' placeholder='Last Name' value={SignUpData.lastName} onChange={handleChange}/>
-      Email <input type='email' name='email' placeholder='Email' value={SignUpData.email} onChange={handleChange}/>
-      Password <input type='password' name='password' placeholder='Password' value={SignUpData.password} onChange={handleChange}/>
+      <li> First Name <input type='text' name='firstName' placeholder='First Name' value={SignUpData.firstName} onChange={handleChange}/></li>
+      <li> Last Name <input type='text' name='lastName' placeholder='Last Name' value={SignUpData.lastName} onChange={handleChange}/></li>
+      <li>  Email <input type='email' name='email' placeholder='Email' value={SignUpData.email} onChange={handleChange}/></li>
+      <li> Password <input type='password' name='password' placeholder='Password' value={SignUpData.password} onChange={handleChange}/></li>
+      <li>Confirm Password <input type='password' name='confirmPassword' placeholder='Confrim your password' value={SignUpData.confirmPassword} onChange={handleChange} /></li>
 
       <button type='submit'>Sign Up!</button> {}
       </form>
-
+      </ul>
 {error && <p style={{ color:'red' }}>{error}</p>}
 {success && <p style={{ color: 'green'}}>{success}</p>} 
 
--- server/src/routes/signUpRoute.js
+++ server/src/routes/signUpRoute.js
@@ -11,7 +11,7 @@ const hashedPassword = await bcrypt.hash(password, 10);
 
 const newUser = new User({ firstName, lastName, email, password: hashedPassword });
 await newUser.save();
-res.status(201).send('Successfully Created User Account')
+res.status(201).json({ message: 'Successfully Created User Account'});
     
 });
 
