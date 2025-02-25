2e20260c0609fc7e5062f0c57a8fd44269eeb45d
GeeksforGeeks <rlmagtira@gmail.com>
2025-02-25 22:07:34 +0800

    Front end progress
    // Todo, weather, and news are working
    NEXT:  edit designs and interface
    // Other fixes

---
+++ client/src/APIs/NewsAPI.js
@@ -0,0 +1,4 @@
+export const nAPI_URL=`https://newsapi.org/v2/everything`
+
+export const nAPI_KEY=`75bee3a37c9e4fe5b9a56aca9eda806e`
+
--- client/src/Components/News.jsx
+++ client/src/Components/News.jsx
@@ -1,27 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { nAPI_URL, nAPI_KEY } from '../APIs/NewsAPI';
+import axios from 'axios';
 
 const News = () => {
+  const [topic, setTopic] = useState('');
+  const [articles, setArticles] = useState([]); // Changed to an empty array
+
+  const getNews = async () => {
+    try { 
+      const response = await axios.get(`${nAPI_URL}?q=${topic}&apiKey=${nAPI_KEY}`);
+      const newData = response.data.articles;
+      setArticles(newData); // Corrected setArticles
+    } catch (error) {
+      console.error("Error fetching news:", error); 
+    }  
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    getNews();
+  };
+
   return (
     <div>
-      News here
+      <h1>News here</h1>
 
-<input type='text' name='topic' placeholder='Search Topic'/>
+      <form onSubmit={handleSearch}>
+        <input 
+          type='text' 
+          name='topic' 
+          placeholder='Search Topic' 
+          value={topic} 
+          onChange={(e) => setTopic(e.target.value)}
+        />
+        <button type='submit'>Search News</button>
+      </form>
 
-<di>
-<ul>
-<li>Title</li>
-<li>image</li>
-<li>overview</li>
-<li>source / link</li>
-  
-  </ul> 
+      <div>
+        <ul>
+          {articles.map((article, index) => (
+            <li key={index}>
+              <h3>{article.title}</h3>
+              <img src={article.urlToImage} alt={article.title} style={{ width: '100px' }} /> 
+              <p>{article.description}</p>
+              <a href={article.url} target='_blank' rel='noopener noreferrer'>Read more</a>
+            </li>
+          ))}
+        </ul>
+      </div>
 
-
-</di>
-
-
+      <div>
+        <ul>
+          <li>Title</li>
+          <li>Image</li>
+          <li>Overview</li>
+          <li>Source / Link</li>
+        </ul> 
+      </div>
     </div>
-  )
+  );
 }
 
-export default News
+export default News;
